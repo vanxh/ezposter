074a65ff42789a3b17032f885fd17dd13008c68c
vanxh <hello@vanxh.dev>
2023-06-19 02:56:03 +0530

    app nav bar

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
 
+  images: {
+    domains: ["images.clerk.dev"],
+  },
+
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,12 +1,16 @@
 import { type NextPage } from "next";
 import { useUser } from "@clerk/nextjs";
 
+import { AppNavBar } from "@/components/AppNavBar";
+
 const Page: NextPage = () => {
   const { user } = useUser();
   console.log(user);
 
   return (
-    <div>
+    <div className="container flex flex-row gap-x-6">
+      <AppNavBar />
+
       <div className="text-lg">Your page&apos;s content can go here.</div>
     </div>
   );
