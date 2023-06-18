0237f7b55f7c838d1e4d0ed0c94940f32bc4f022
vanxh <hello@vanxh.dev>
2023-06-19 01:14:23 +0530

    user prisma schema

---
+++ prisma/zod/user.ts
@@ -0,0 +1,18 @@
+import * as z from "zod"
+import { PremiumTier } from "@prisma/client"
+
+export const UserModel = z.object({
+  id: z.number().int(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  premiumTier: z.nativeEnum(PremiumTier),
+  premiumValidUntil: z.date().nullish(),
+  gameflipApiKey: z.string().nullish(),
+  gameflipApiSecret: z.string().nullish(),
+  gameflipId: z.string().nullish(),
+  autoPost: z.boolean(),
+  postTime: z.number().int().nullish(),
+  purgeOlderThan: z.number().int().nullish(),
+  nPosted: z.number().int(),
+  nPurged: z.number().int(),
+})
