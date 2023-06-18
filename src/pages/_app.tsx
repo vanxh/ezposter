7e677951a055daddddca0bb15112d0784db529af
vanxh <hello@vanxh.dev>
2023-06-19 00:02:03 +0530

    setup clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <div className="flex h-[100vh] w-full flex-col items-center justify-center">
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
---
+++ src/pages/example.tsx
@@ -0,0 +1,13 @@
+// pages/example.tsx
+import { UserButton } from "@clerk/nextjs";
+
+export default function Example() {
+  return (
+    <>
+      <header>
+        <UserButton afterSignOutUrl="/" />
+      </header>
+      <div>Your page&apos;s content can go here.</div>
+    </>
+  );
+}
