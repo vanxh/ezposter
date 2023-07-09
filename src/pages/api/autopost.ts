fde465862b013c9c895547f6f796543d5673e1cf
vanxh <hello@vanxh.dev>
2023-07-09 17:57:31 +0530

    trycatch in autopost

--- src/pages/api/autopost.ts
+++ src/pages/api/autopost.ts
@@ -9,55 +9,64 @@ import { createListingQuery } from "@/utils/gfapi";
 export default Queue("api/autopost", async (userId: number) => {
   console.log(`[${new Date().toLocaleTimeString()}] User ${userId} auto post`);
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
-  if (!user || !isPremium(user) || !user.autoPost || !isGameflipConnected(user))
-    return;
-
-  const nListings = await prisma.gameflipListing.count({
-    where: {
-      userId: user.id,
-      autoPost: true,
-    },
-  });
-  if (!nListings) return;
-
-  const listing = (
-    await prisma.$queryRawUnsafe<GameflipListing[]>(
-      `SELECT * FROM GameflipListing WHERE userId = ${user.id} AND autoPost = true ORDER BY RAND() LIMIT 1`
-    )
-  )[0];
-  if (!listing) return;
-
-  const listingQuery = createListingQuery(listing, {
-    gameflipApiKey: user.gameflipApiKey as string,
-    gameflipApiSecret: user.gameflipApiSecret as string,
-    gameflipId: user.gameflipId as string,
-  });
-
-  const gfapi = new GFApi({
-    gameflipApiKey: user.gameflipApiKey as string,
-    gameflipApiSecret: user.gameflipApiSecret as string,
-    gameflipId: user.gameflipId as string,
-  });
-  const listingId = await gfapi.postListing(
-    listingQuery,
-    listing.images as string[]
-  );
-
-  await prisma.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      nPosted: {
-        increment: 1,
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
       },
-    },
-  });
+    });
+    if (
+      !user ||
+      !isPremium(user) ||
+      !user.autoPost ||
+      !isGameflipConnected(user)
+    )
+      return;
 
-  console.log(`Auto post job for ${user.id}: posted listing ${listingId}`);
+    const nListings = await prisma.gameflipListing.count({
+      where: {
+        userId: user.id,
+        autoPost: true,
+      },
+    });
+    if (!nListings) return;
+
+    const listing = (
+      await prisma.$queryRawUnsafe<GameflipListing[]>(
+        `SELECT * FROM GameflipListing WHERE userId = ${user.id} AND autoPost = true ORDER BY RAND() LIMIT 1`
+      )
+    )[0];
+    if (!listing) return;
+
+    const listingQuery = createListingQuery(listing, {
+      gameflipApiKey: user.gameflipApiKey as string,
+      gameflipApiSecret: user.gameflipApiSecret as string,
+      gameflipId: user.gameflipId as string,
+    });
+
+    const gfapi = new GFApi({
+      gameflipApiKey: user.gameflipApiKey as string,
+      gameflipApiSecret: user.gameflipApiSecret as string,
+      gameflipId: user.gameflipId as string,
+    });
+    const listingId = await gfapi.postListing(
+      listingQuery,
+      listing.images as string[]
+    );
+
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        nPosted: {
+          increment: 1,
+        },
+      },
+    });
+
+    console.log(`Auto post job for ${user.id}: posted listing ${listingId}`);
+  } catch (e) {
+    console.error(`Auto post job for ${userId} failed: ${e as string}`);
+  }
 });
--- src/pages/api/autopurge.ts
+++ src/pages/api/autopurge.ts
@@ -10,54 +10,63 @@ const wait = promisify(setTimeout);
 export default Queue("api/autopurge", async (userId: number) => {
   console.log(`[${new Date().toLocaleTimeString()}] User ${userId} auto purge`);
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
-  if (!user || !isPremium(user) || !user.autoPost || !isGameflipConnected(user))
-    return;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (
+      !user ||
+      !isPremium(user) ||
+      !user.autoPost ||
+      !isGameflipConnected(user)
+    )
+      return;
 
-  const gfapi = new GFApi({
-    gameflipApiKey: user.gameflipApiKey as string,
-    gameflipApiSecret: user.gameflipApiSecret as string,
-  });
-  const listings = await gfapi.searchListings({
-    owner: user.gameflipId as string,
-    status: "onsale",
-    sort: "created:asc",
-    visibility: "public",
-    limit: "50",
-    created: `,${new Date(
-      Date.now() - user.purgeOlderThan * 60 * 1000
-    ).toISOString()}`,
-  });
+    const gfapi = new GFApi({
+      gameflipApiKey: user.gameflipApiKey as string,
+      gameflipApiSecret: user.gameflipApiSecret as string,
+    });
+    const listings = await gfapi.searchListings({
+      owner: user.gameflipId as string,
+      status: "onsale",
+      sort: "created:asc",
+      visibility: "public",
+      limit: "50",
+      created: `,${new Date(
+        Date.now() - user.purgeOlderThan * 60 * 1000
+      ).toISOString()}`,
+    });
 
-  for await (const listing of listings) {
-    try {
-      await gfapi.deleteListing(listing.id);
+    for await (const listing of listings) {
+      try {
+        await gfapi.deleteListing(listing.id);
 
-      await prisma.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          nPurged: {
-            increment: 1,
+        await prisma.user.update({
+          where: {
+            id: user.id,
           },
-        },
-      });
-    } catch (e) {
-      console.error(
-        `Auto purge job for ${user.id} failed to delete listing ${
-          listing.id
-        }: ${e as string}`
-      );
+          data: {
+            nPurged: {
+              increment: 1,
+            },
+          },
+        });
+      } catch (e) {
+        console.error(
+          `Auto purge job for ${user.id} failed to delete listing ${
+            listing.id
+          }: ${e as string}`
+        );
+      }
+      await wait(1000);
     }
-    await wait(1000);
-  }
 
-  console.log(
-    `Auto purge job for ${user.id}: Deleted ${listings.length} listings`
-  );
+    console.log(
+      `Auto purge job for ${user.id}: Deleted ${listings.length} listings`
+    );
+  } catch (e) {
+    console.error(`Auto purge job for ${userId} failed: ${e as string}`);
+  }
 });
