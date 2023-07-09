0322a50f074cc0a986113cb30291eb3c60d33b88
vanxh <hello@vanxh.dev>
2023-07-09 16:56:20 +0530

    post listing

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -2,7 +2,16 @@ import { type NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import { type GameflipListing } from "@prisma/client";
-import { DollarSign, Edit, MinusCircle, PlusCircle, Trash } from "lucide-react";
+import {
+  DollarSign,
+  Edit,
+  MinusCircle,
+  Play,
+  PlusCircle,
+  Power,
+  PowerOff,
+  Trash,
+} from "lucide-react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { formatDuration, intervalToDuration } from "date-fns";
 
@@ -10,7 +19,6 @@ import { api } from "@/utils/api";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Switch } from "@/components/ui/switch";
 import { showToast } from "@/components/ui/use-toast";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
@@ -84,6 +92,18 @@ const Page: NextPage = () => {
     },
   });
 
+  const { mutateAsync: postListing } = api.user.listing.post.useMutation({
+    onSuccess: (listingId) => {
+      void navigator.clipboard.writeText(
+        `https://gameflip.com/item/${listingId}`
+      );
+      showToast("Posted listing and copied link to clipboard!");
+    },
+    onError: (e) => {
+      showToast(`${e.message ?? e ?? "Unknown error while posting listing"}`);
+    },
+  });
+
   const { mutateAsync: enableListing } = api.user.listing.enable.useMutation({
     onMutate: ({ id }) => {
       updateListingsCache(id, {
@@ -132,7 +152,7 @@ const Page: NextPage = () => {
       </div>
 
       {user && (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-4">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
@@ -212,7 +232,10 @@ const Page: NextPage = () => {
       >
         <div className="grid grid-cols-2 gap-x-6 gap-y-6 md:grid-cols-4 lg:grid-cols-6">
           {listings.map((l) => (
-            <div key={l.id} className="flex flex-col gap-y-4">
+            <div
+              key={l.id}
+              className="flex flex-col gap-y-4 rounded-lg border border-border px-4 py-4"
+            >
               <Image
                 src={(l.images as string[])?.[0] || ""}
                 alt="listing image"
@@ -229,23 +252,31 @@ const Page: NextPage = () => {
                 </span>
               </div>
 
-              <div className="flex flex-row items-center justify-between gap-x-4 md:justify-normal">
+              <div className="flex flex-row items-center justify-between gap-x-4 md:justify-between">
                 <Link href={`/app/listings/${l.id}`}>
-                  <Edit size={20} />
+                  <Edit size={16} />
                 </Link>
-                <Trash
-                  size={20}
-                  className="cursor-pointer"
-                  onClick={() => void deleteListing({ id: l.id })}
-                />
-                <Switch
-                  checked={l.autoPost}
-                  onCheckedChange={(c) =>
-                    void (c
-                      ? enableListing({ id: l.id })
-                      : disableListing({ id: l.id }))
-                  }
-                />
+                <button onClick={() => void deleteListing({ id: l.id })}>
+                  <Trash size={16} />
+                </button>
+                <button onClick={() => void postListing({ id: l.id })}>
+                  <Play size={16} />
+                </button>
+                <button
+                  onClick={() => {
+                    if (l.autoPost) {
+                      void disableListing({ id: l.id });
+                    } else {
+                      void enableListing({ id: l.id });
+                    }
+                  }}
+                >
+                  {l.autoPost ? (
+                    <Power className="h-4 w-4" />
+                  ) : (
+                    <PowerOff className="h-4 w-4" />
+                  )}
+                </button>
               </div>
             </div>
           ))}
