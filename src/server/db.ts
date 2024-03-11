0f730773260dac34690b6f71ea0ccb8157285dfe
vanxh <hello@vanxh.dev>
2024-03-12 03:29:23 +0530

    use turso db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 import { env } from "../env.mjs";
+
+const libsql = createClient({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -10,6 +19,7 @@ export const prisma =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter,
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
